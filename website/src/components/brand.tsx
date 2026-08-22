2ccb2d8d79d796b65dd5a2877c6beec134bbe1fa
Arata <aratadayoon@yahoo.co.jp>
2026-08-22 11:35:09 +0900

    fix: restore approved asc branding

--- website/src/components/brand.tsx
+++ website/src/components/brand.tsx
@@ -3,10 +3,9 @@ import Link from "next/link";
 export function Brand() {
   return (
     <Link className="brand" href="/" aria-label="ascdir home">
-      <span className="brandMark" aria-hidden="true">
+      <span className="brandWordmark" aria-hidden="true">
         asc
       </span>
-      <span>ascdir</span>
     </Link>
   );
 }
